3c97773ef3dc826540527f57baa49effea338228
faleksic <filipp.aleksic@gmail.com>
2016-10-26 15:43:46 +0200

    Started creating SpreadsheetConditionalFormatting class and created method for conditional formatting minimum and maximum value between color range of two colors and method for writing using OpenXmlWriter

---
+++ Acceleratio.XCellKit.Tests/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+
+namespace Acceleratio.XCellKit.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ConditionalFormattingRule rule = new ConditionalFormattingRule() { Type = ConditionalFormatValues.ColorScale, Priority = 1 };
+            SpreadsheetConditionalFormatting format = new SpreadsheetConditionalFormatting("A1:A5", rule);
+            Color first = new Color() { Rgb = "FFFFEF9C" };
+            Color second = new Color() { Rgb = "EEEEEF9C" };
+            format.formatBetweenMinAndMax(first, second);
+            
+        }
+    }
+}
---
+++ Acceleratio.XCellKit/SpreadsheetConditionalFormatting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+
+namespace Acceleratio.XCellKit
+{
+    public class SpreadsheetConditionalFormatting
+    {
+        private ConditionalFormatting conditionalFormatting;
+        private ConditionalFormattingRule conditionalRule;
+
+        public SpreadsheetConditionalFormatting(string reference, ConditionalFormattingRule rule)
+        {
+            conditionalRule = rule;
+            conditionalFormatting = new ConditionalFormatting() { SequenceOfReferences = new ListValue<StringValue>() { InnerText = reference } };
+        }
+
+        public ConditionalFormatting formatBetweenMinAndMax(Color first, Color second)
+        {
+            ColorScale colorScale = new ColorScale();
+            ConditionalFormatValueObject conditionalFormatValueObject1 = new ConditionalFormatValueObject() { Type = ConditionalFormatValueObjectValues.Min};
+            ConditionalFormatValueObject conditionalFormatValueObject2 = new ConditionalFormatValueObject() { Type = ConditionalFormatValueObjectValues.Max };
+            colorScale.Append(conditionalFormatValueObject1);
+            colorScale.Append(conditionalFormatValueObject2);
+            colorScale.Append(first);
+            colorScale.Append(second);
+            conditionalRule.Append(colorScale);
+            conditionalFormatting.Append(conditionalRule);
+            return conditionalFormatting;
+        }
+
+        public void writeConditionalFormats(OpenXmlWriter writer)
+        {
+            writer.WriteStartElement(new ConditionalFormatting());
+            writer.WriteElement(conditionalFormatting);
+            writer.WriteEndElement();
+        }
+
+
+    }
+}

e0f6119d8eda20a8fbc9c3f99d811c19387740b5
Vinko Bedek <VinkoBedek@hotmail.com>
2018-09-26 08:32:23 +0200

    moved the spreadsheetTable classes to a separate folder

---
+++ Acceleratio.XCellKit/Tables/RequestTableRowEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acceleratio.XCellKit.Tables
+{
+    public class RequestTableRowEventArgs : EventArgs
+    {
+        public SpreadsheetRow Row { get; set; }
+        public bool Finished { get; set; }
+    }
+}

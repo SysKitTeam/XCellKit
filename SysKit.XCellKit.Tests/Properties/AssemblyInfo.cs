1a09ead874c6ec78d25c78bab831f99beebd7acb
Vinko Bedek <VinkoBedek@hotmail.com>
2018-09-28 11:14:33 +0200

    assembly info changes

--- SysKit.XCellKit.Tests/Properties/AssemblyInfo.cs
+++ SysKit.XCellKit.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Acceleratio.xExport.Tests")]
+[assembly: AssemblyTitle("SysKit.XCellKit.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Acceleratio.xExport.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("SysKit Ltd.")]
+[assembly: AssemblyCopyright("Copyright © SysKit Ltd. 2018")]
+[assembly: AssemblyProduct("SysKit.XCellKit.Tests")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
